fc2e22ef63201114ca8d95788d56124233dc2711
eduardo106919 <a106919@alunos.uminho.pt>
2026-05-05 16:37:55 +0100

    add: init dependencies

---
+++ src/punchcard/lexer/__init__.py
@@ -0,0 +1,4 @@
+from punchcard.lexer.lexer import PunchCardLexer
+from punchcard.lexer.definitions import KEYWORDS, TOKENS, LITERALS
+
+__all__ = ["PunchCardLexer", "KEYWORDS", "TOKENS", "LITERALS"]
