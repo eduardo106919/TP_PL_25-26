aa4200f4bcb7bb722f4a4062a857b7d87be6962e
goncalo106842 <a106842@uminho.pt>
2026-05-14 22:09:46 +0100

    test: update custom test 09_subroutine

--- test/custom/09_subroutine.f
+++ test/custom/09_subroutine.f
@@ -1,13 +1,22 @@
-      PROGRAM SUBTEST
-      INTEGER X, Y, RES
-      X = 8
-      Y = 3
-      CALL SOMA(X, Y, RES)
-      PRINT *, 'Soma: ', RES
+      PROGRAM STARWARS
+      INTEGER REP
+      REP = 3
+      
+      CALL KENOBI(REP)
+      CALL VADER(REP)
+
       END
 
-      SUBROUTINE SOMA(A, B, C)
-      INTEGER A, B, C
-      C = A + B
-      RETURN
+      SUBROUTINE KENOBI(REP)
+      INTEGER REP, I
+      DO 10 I = 1, REP
+            PRINT *, 'Hello there!'
+   10 CONTINUE
+      END
+
+      SUBROUTINE VADER(REP)
+      INTEGER REP, I
+      DO 20 I = 1, REP
+            PRINT *, 'i am your father'
+   20 CONTINUE
       END
